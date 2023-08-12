88ed5eb9911bfe0042c533b90a09ec14d81ca2b5
suriya216 <136613620+suriya216@users.noreply.github.com>
2023-08-12 11:27:01 +0530

    Create Aug12.java

---
+++ Aug12.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String str=sc.next();
+		String c=sc.next();
+		str=str.replace(c,"");
+		System.out.print(str);
+	}
+}
