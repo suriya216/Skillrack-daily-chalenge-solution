3bedbdf3360dbe4c392185a0441098673306cc7d
suriya216 <136613620+suriya216@users.noreply.github.com>
2024-01-10 14:57:34 +0530

    Jan/10/24

--- Jan1024.java
+++ Jan1024.java
@@ -1,33 +1,14 @@
 import java.util.*;
+
 public class Hello {
 
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int N=sc.nextInt();
-        int [][]arr=new int[N][N];
-        int odd = 1, even = 2;
-        for(int j=0;j<N;j++){
-            boolean f = true;
-            for(int i=0;i<N;i++){
-                if(i==j){
-                    arr[i][j]=0;
-                    f=false;
-                }
-                else if(!f){
-                    arr[i][j]=odd;
-                    odd+=2;
-                }else{
-                    arr[i][j]=even;
-                    even+=2;
-                }
-            }
-        }
-        for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++){
-                System.out.print(arr[i][j]+" ");
-            }
-            System.out.println();
-        }
-        sc.close();
+		Scanner sc=new Scanner(System.in);
+		double M=sc.nextDouble();
+		double N=sc.nextDouble();
+		int X=sc.nextInt();
+		String format="%."+X+"f";
+		System.out.print(String.format(format,M/N));
+		sc.close();
 	}
 }
