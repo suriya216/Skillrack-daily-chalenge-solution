d4d2c035345a159903fe7fa7de60848151a95a88
suriya216 <136613620+suriya216@users.noreply.github.com>
2024-02-14 09:34:00 +0530

    Feb/14/24

---
+++ Feb1424.java
@@ -0,0 +1,13 @@
+   int t=N;
+    int max=0, smax=0;
+    while(t!=0){
+        int l=t%10;
+        if(l>max){
+            smax=max;
+            max=l;
+        }
+        if(smax<l && l!=max){
+            smax=l;
+        }
+        t/=10;
+    }
